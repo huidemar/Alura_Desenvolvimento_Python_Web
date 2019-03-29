aab9094f70fd06fa5840f4af1856a2b2bffdd9bb
Huidemar <huidemar@gmail.com>
2019-03-28 22:45:57 -0300

    Curso Introducao Orientacao a Objetos - Python

---
+++ CursoPython3Intro_POO/conta.py
@@ -0,0 +1,55 @@
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo o objeto {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor passou o limite")
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = (self.saldo + self.limite)
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def extrato(self):
+        print("Títular: {}".format(self.titular()))
+        print("Saldo: {}".format(self.saldo()))
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
